ad9cba824d0accb9b75853d37a3ddc7594175f36
macabeus <bruno.macabeus@gmail.com>
2022-04-09 20:39:50 +0100

    components: add seo

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,13 @@
 <script>
   import Footer from '../components/Footer.svelte'
+  import Seo from '../components/Seo.svelte'
   import Header from '../components/Header.svelte'
   import '../app.css'
 </script>
 
 <svelte:head>
   <title>GambiConf - The Blow Your Mind Conference | 3rd and 10th of September</title>
+  <Seo />
 </svelte:head>
 
 <Header />

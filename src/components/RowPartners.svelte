d63dea32d854909b4bb141d828362ec8cfe63750
macabeus <bruno.macabeus@gmail.com>
2023-06-14 17:56:28 -0300

    partners: add codelab

--- src/components/RowPartners.svelte
+++ src/components/RowPartners.svelte
@@ -2,11 +2,20 @@
   import { Localized } from "@nubolab-ffwd/svelte-fluent"
   import { assets } from "$app/paths"
   import { t } from "../store/locale"
+  import { theme } from "../store/theme"
   import Window from "./Window.svelte"
 </script>
 
 <Window title={$t("partners--title")}>
   <div class="content">
+    <a
+      href="https://codelab.ime.usp.br/"
+      target="_blank"
+      rel="noopener"
+    >
+      <img src={`${assets}/codelab-${$theme}.svg`} alt="Codelab" />
+    </a>
+
     <Localized id="partners--become-a-partner" args={{ assets }} let:attrs>
       <a
         href="https://docs.google.com/presentation/d/1beZavMrKfFFwblVRevZbxyq9UH4rS717q9oJrTVOCxI/edit?usp=sharing"
@@ -22,7 +31,7 @@
 <style>
   .content {
     display: grid;
-    grid-template-columns: 1fr;
+    grid-template-columns: repeat(2, 1fr);
     justify-items: center;
     gap: 25px;
 
@@ -30,18 +39,12 @@
   }
 
   .content img {
-    max-height: 100px;
+    height: 175px;
     transition: filter 0.75s;
     filter: grayscale(1);
   }
 
   .content img:hover {
     filter: grayscale(0);
-  }
-
-  @media screen and (min-width: 768px) {
-    .content img {
-      max-height: 175px;
-    }
   }
 </style>

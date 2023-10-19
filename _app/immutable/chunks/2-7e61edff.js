489c7b4fcc6598682004397bbb60856e2211f7a3
macabeus <bruno.macabeus@gmail.com>
2023-10-19 17:28:54 +0100

    Updates

--- _app/immutable/chunks/2-7e61edff.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-6fdef134.js";export{t as component};
---
+++ _app/immutable/chunks/2-ae3c213b.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-49c074bd.js";export{t as component};

16fbeb28eb2b8c92875c04b845f362f787457b99
LuckShiba <LuckShiba@protonmail.com>
2024-05-07 22:50:03 -0300

    seo: use texts in portuguese

--- src/components/Seo.svelte
+++ src/components/Seo.svelte
@@ -1,37 +1,37 @@
 <meta
   name="description"
-  content="GambiConf - The Blow Your Mind Conference / September 3 and 10"
+  content="GambiConf - The Blow Your Mind Conference / 02 e 03 de novembro"
 />
 
 <meta
   property="og:title"
-  content="GambiConf - The Blow Your Mind Conference / September 3 and 10"
+  content="GambiConf - The Blow Your Mind Conference / 02 e 03 de novembro"
 />
 <meta property="og:url" content="https://gambiconf.dev/" />
 <meta
   property="og:description"
-  content="GambiConf is a tech conference that's gonna blow your mind! This is a multilingual conference, not limited to a specific technology. We focus on the purpose of the presented projects, their unique nature, and what one can learn from a random, very unusual, idea."
+  content="O evento GambiConf é um ambiente amistoso, de boa convivência, inclusivo e livre de intimidação, onde todas as pessoas são bem-vindas e a civilidade é exigida."
 />
 <meta property="og:image" content="https://gambiconf.dev/favicon.png" />
 <meta property="og:type" content="website" />
 <meta
   property="og:image:alt"
-  content="GambiConf's mascot. An anthropomorphic orange monkey using a neck scarf with the flag of the European Union"
+  content="O mascote da GambiConf, um mico-leão dourado antropomórfico."
 />
 
 <meta name="twitter:card" content="summary" />
 <meta
   name="twitter:title"
-  content="GambiConf - The Blow Your Mind Conference / September 3 and 10"
+  content="GambiConf - The Blow Your Mind Conference / 02 e 03 de novembro"
 />
 <meta name="twitter:url" content="https://gambiconf.dev/" />
 <meta
   name="twitter:description"
-  content="GambiConf is a tech conference that's gonna blow your mind! This is a multilingual conference, not limited to a specific technology. We focus on the purpose of the presented projects, their unique nature, and what one can learn from a random, very unusual, idea."
+  content="O evento GambiConf é um ambiente amistoso, de boa convivência, inclusivo e livre de intimidação, onde todas as pessoas são bem-vindas e a civilidade é exigida."
 />
 <meta name="twitter:image" content="https://gambiconf.dev/favicon.png" />
 <meta
   name="twitter:image:alt"
-  content="GambiConf's mascot. An anthropomorphic orange monkey using a neck scarf with the flag of the European Union"
+  content="O mascote da GambiConf, um mico-leão dourado antropomórfico."
 />
 <meta name="twitter:creator" content="@gambiconf" />

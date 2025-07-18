cd760317a3a8f90832e236057904523e81f02433
macabeus <bruno.macabeus@gmail.com>
2025-07-18 12:56:16 +0100

    Updates

--- _app/immutable/entry/start.BYgbIx6j.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/Dgd1M8_v.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.DeG-Dj-4.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/CC7w81VK.js";export{o as load_css,r as start};

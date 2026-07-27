611764d01021e19127cbe235b251960535c17455
macabeus <bruno.macabeus@gmail.com>
2026-07-27 10:26:08 +0100

    Updates

--- _app/immutable/entry/start.BsBG7M0e.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BlCQEwW7.js";import{C as t}from"../chunks/gW5-3IUT.js";export{t as load_css,r as start};
---
+++ _app/immutable/entry/start.VCVKYS1A.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/CFPnda36.js";import{C as t}from"../chunks/zCpzVo0V.js";export{t as load_css,r as start};

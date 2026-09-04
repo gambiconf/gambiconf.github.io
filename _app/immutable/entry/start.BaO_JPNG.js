74460efb3802c90b0ceb4f26395eb60097ad381c
macabeus <bruno.macabeus@gmail.com>
2026-09-04 16:14:10 +0100

    Updates

--- _app/immutable/entry/start.BaO_JPNG.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/Z1K7nKOo.js";import{C as t}from"../chunks/CrZAoIta.js";export{t as load_css,r as start};
---
+++ _app/immutable/entry/start.CzxdSpMG.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/BpUfFKFV.js";import{C as t}from"../chunks/BLwtqUB1.js";export{t as load_css,r as start};
